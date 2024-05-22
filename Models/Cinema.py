6616a571f9a853fa626a94cddd58e80a0d5c549c
Fany Parama Admaja <admaja404@gmail.com>
2024-05-22 11:54:22 +0700

    delete Models

--- Models/Cinema.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import mapped_column, relationship
-from connection import Base
-
-class Cinema(Base): 
-    __tablename__ = 'cinema'
-    id = mapped_column(Integer, primary_key=True)
-    name = mapped_column(String(100))
-    city = mapped_column(Integer)
